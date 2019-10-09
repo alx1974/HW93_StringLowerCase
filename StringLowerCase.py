a88ff8da3ab8bded4e8ebb4d36bc0599e147a32a
Alexander Gaco <alexander.gaco@gmail.com>
2019-10-09 21:45:42 +0200

    String Lower Case Version 1

---
+++ StringLowerCase.py
@@ -0,0 +1,7 @@
+# HW9.3 Make String lowercase
+str1 = "Das ist ein Test"
+str2 = "Today Is A Beautiful DAY"
+lower_str1 = str1.lower()
+lower_str2 = str2.lower()
+print(lower_str1)
+print(lower_str2)
